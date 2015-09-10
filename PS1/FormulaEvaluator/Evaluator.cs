b517218c67ba6eaddac742084c57f06e93b8be6d
programmIt <supermultiplet@hotmail.com>
2015-09-09 23:25:59 -0600

    This is the set up commit

---
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluator
+{
+    /// <summary>
+    /// This is the first class
+    /// </summary>
+    public class Evaluator
+    {
+        /// <summary>
+        /// This is a delegate that you can use
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public delegate int Lookup(String s);
+
+        public static int Evaluate(String s, Lookup varEvaluator)
+        {
+            string[] substrings = Regex.Split(s, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            return 0;
+        }
+
+        
+    }
+}
