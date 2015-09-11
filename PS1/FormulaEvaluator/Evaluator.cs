772f8544f593aa70c5ea3c95d9b1c846c6e99cfd
LeGrange <mlancegrange@gmail.com>
2015-09-10 23:02:08 -0600

    added XML documentation and figured some other things out.

--- PS1/FormulaEvaluator/Evaluator.cs
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -8,26 +8,19 @@ using System.Threading.Tasks;
 namespace FormulaEvaluator
 {
     /// <summary>
-    /// This is the first class
+    /// This is the static Evaluator Class. Containing 
+    /// the necessary methods to complete PS1
     /// </summary>
-    public class Evaluator
+    public static class Evaluator
     {
         /// <summary>
-        /// This is a delegate that you can use
+        /// Lookup delegate holds the value that will be 
         /// </summary>
         /// <param name="s"></param>
         /// <returns></returns>
         public delegate int Lookup(String s);
 
-        /// <summary>
-        /// This is a stack that will contain the operators
-        /// </summary>
-        public Stack<double> operatorStack = new Stack<double>();
-
-        /// <summary>
-        /// This is a stack that will contain the numbers
-        /// </summary>
-        public Stack<int> numberStack = new Stack<int>();
+        
 
         /// <summary>
         /// This is the Evaluate Method
@@ -37,12 +30,54 @@ namespace FormulaEvaluator
         /// <returns></returns>
         public static int Evaluate(String s, Lookup varEvaluator)
         {
+            
+            Stack<string> operatorStack = new Stack<string>();
+
+            Stack<double> numberStack = new Stack<double>();
+            try{
             //This helps to split strings to the proper operator.
             string[] substrings = Regex.Split(s, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
-            
+
+            //run through each string and do things to it.
+            foreach(String t in substrings)
+            {
+                if (isVar(t))
+                {
+
+                }
+                else if (isInt(t))
+                {
+
+                }
+                else if (isOP(t))
+                {
+
+                }
+            }
+            //if (substrings[i] is number)
+            //{
+            //    //put on number stack
+            //}
+            //else if(substrings[i] is operator)
+            //{
+            //    //put on operator stack
+            //}
+            }catch(ArgumentException){
+
+            }
             return 0;
         }
 
+        private static bool isVar(String v)
+        {
+            return Regex.IsMatch(v, "(^[a-z]|[A-Z]) + \\d+$");
+        }
+
         
+    }
+
+    public static class Extensions 
+    {
+ 
     }
 }
