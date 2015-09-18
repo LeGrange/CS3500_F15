6ead2533ea0936ed877ecf43abea2c1ba4616c7a
LeGrange <mlancegrange@gmail.com>
2015-09-17 21:22:53 -0600

    Added Test Cases from Course_Examples

--- PS2/DevelopmenTests/DevelopmentTests.cs
+++ PS2/DevelopmenTests/DevelopmentTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DevelopmenTests
 {
     [TestClass]
-    public class UnitTest1
+    public class DevelopmentTests
     {
         [TestMethod]
         public void TestMethod1()
